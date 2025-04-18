27d620370d66cb8e5aed91f10d7456abe041c5f6
Bakhteev <Bakhteev23@inbox,ru>
2025-04-18 11:01:29 +0300

    KS-1-2

--- src/main/kotlin/Lesson_1/Zadacha2.kt
+++ src/main/kotlin/Lesson_1/Zadacha2.kt
@@ -1,17 +1,15 @@
- package Lesson_1
+package Lesson_1
 
- fun main() {
+fun main() {
 
-    println("Решение второй задачи")
+   val numberOfOrders = 75
+   val thanksForYourOrder = "Спасибо за Ваш заказ.Ждем Вас снова!"
+   var totalEmployees = 2000
 
-    val numberOfOrders = 75
-    val thanksForYourOrder = "Спасибо за Ваш заказ.Ждем Вас снова!"
-    var totalEmployees = 2000
+   println("Заказов сегодня : $numberOfOrders")
+   println(thanksForYourOrder)
+   println("Общее колличество работников : $totalEmployees")
 
-    println("Заказов сегодня : $numberOfOrders")
-    println(thanksForYourOrder)
-    println("Общее колличество работников : $totalEmployees")
-
-    totalEmployees = 1999
-    println("Общее колличество работников после увольнения : $totalEmployees")
- }
+   totalEmployees -= 1
+   println("Общее колличество работников после увольнения : $totalEmployees")
+}
