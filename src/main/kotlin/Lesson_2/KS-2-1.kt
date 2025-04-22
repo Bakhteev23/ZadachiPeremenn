6d6f973a5fdb8640565d7469949d3bce24100b42
Bakhteev <Bakhteev23@inbox,ru>
2025-04-22 13:35:43 +0300

    KS-2-1

---
+++ src/main/kotlin/Lesson_2/KS-2-1.kt
@@ -0,0 +1,11 @@
+package Lesson_2
+
+fun main () {
+
+    val floatA: Float = 3.00f
+    val floatB: Float = 4.00f
+    val floatC: Float = 3.00f
+    val floatD: Float = 5.00f
+    val complexExpression = (floatA + floatB + floatC + floatD) / 4
+    println ( complexExpression)
+}
