36083c947b0e2ec5d1537f8298096d48ba656f94
Bakhteev <Bakhteev23@inbox,ru>
2025-04-25 15:28:57 +0300

    KS-2-1-create

--- src/main/kotlin/Lesson_2/KS-2-1.kt
+++ src/main/kotlin/Lesson_2/KS-2-1.kt
@@ -2,10 +2,12 @@ package Lesson_2
 
 fun main () {
 
-    val floatA: Float = 3.00f
-    val floatB: Float = 4.00f
-    val floatC: Float = 3.00f
-    val floatD: Float = 5.00f
-    val complexExpression = (floatA + floatB + floatC + floatD) / 4
-    println ( complexExpression)
+    val Student1 : Float = 3.00f
+    val Student2 : Float = 4.00f
+    val Student3 : Float = 3.00f
+    val Student4 : Float = 5.00f
+
+    val finalCalculationResult : Float = (Student1 + Student2 + Student3 + Student4) / 4
+
+    println("Средний балл класса: $finalCalculationResult")
 }
