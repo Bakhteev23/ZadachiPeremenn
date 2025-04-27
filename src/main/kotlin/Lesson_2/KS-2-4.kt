f05158c81a9c521f03ba18f7e687615a68502563
Bakhteev <Bakhteev23@inbox,ru>
2025-04-27 21:51:37 +0300

    KS-2-4

---
+++ src/main/kotlin/Lesson_2/KS-2-4.kt
@@ -0,0 +1,14 @@
+package Lesson_2
+
+fun main () {
+
+    val minedCrystalOres: Int = 7
+    val minedIronOre: Int = 11
+    val percentageBaff: Double = 0.20
+
+    val bonusCrystal: Int = (minedCrystalOres * percentageBaff).toInt()
+    val bonusIron: Int = (minedIronOre * percentageBaff).toInt()
+
+    println("Бонусные кристаллы: $bonusCrystal")
+    println("Бонусная железная руда: $bonusIron")
+}
