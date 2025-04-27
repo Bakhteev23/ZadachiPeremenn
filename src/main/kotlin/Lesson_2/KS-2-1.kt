cd7e159fe79c067a54797713bff46be1d3d563c3
Bakhteev <Bakhteev23@inbox,ru>
2025-04-27 21:31:06 +0300

    KS-2-1-create-2

--- src/main/kotlin/Lesson_2/KS-2-1.kt
+++ src/main/kotlin/Lesson_2/KS-2-1.kt
@@ -6,8 +6,9 @@ fun main () {
     val Student2 : Float = 4.00f
     val Student3 : Float = 3.00f
     val Student4 : Float = 5.00f
+    val totalStudentCount : Int = 4
 
-    val finalCalculationResult : Float = (Student1 + Student2 + Student3 + Student4) / 4
+    val finalCalculationResult : Float = (Student1 + Student2 + Student3 + Student4) / totalStudentCount
 
     println("Средний балл класса: $finalCalculationResult")
 }
