e25fef869559277845416a7dcc24b843796f68ac
Bakhteev <Bakhteev23@inbox,ru>
2025-04-30 15:01:59 +0300

    KS-3-2

---
+++ src/main/kotlin/Lesson_3/KS-3-2.kt
@@ -0,0 +1,18 @@
+package Lesson_3
+
+fun main() {
+
+        val familyAt20 = "Андреева"
+        val name = "Татьяна"
+        val partronymic = "Сергеевна"
+        val ageAt20 = 20
+
+        val familyAt22 = "Сидорова"
+        val ageAt22 = 22
+
+    println("[$familyAt20 $name $partronymic, $ageAt20]")
+    println("[$familyAt22 $name $partronymic, $ageAt22]")
+
+
+
+}
