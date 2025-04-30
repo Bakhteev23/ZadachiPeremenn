a608256f68af97a0fd268f416b90781a62bf657c
Bakhteev <Bakhteev23@inbox,ru>
2025-04-30 14:19:23 +0300

    KS-3-1

---
+++ src/main/kotlin/Lesson_3/KS-3-1.kt
@@ -0,0 +1,16 @@
+package Lesson_3
+fun main () {
+
+    val userName = "Paul"
+    var welcome = ""
+
+    welcome = ("Добрый день, $userName")
+    println(welcome)
+
+    welcome = ("Добрый вечер, $userName")
+    println(welcome)
+
+
+
+
+}
