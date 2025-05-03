455134ee359b10f6476747f83d430ad2801ec1a3
Bakhteev <Bakhteev23@inbox,ru>
2025-05-03 22:53:52 +0300

    KS-3-1-create

--- src/main/kotlin/Lesson_3/KS-3-1.kt
+++ src/main/kotlin/Lesson_3/KS-3-1.kt
@@ -1,16 +1,14 @@
 package Lesson_3
+
 fun main () {
 
     val userName = "Paul"
     var welcome = ""
 
-    welcome = ("Добрый день, $userName")
+    welcome = "Добрый день, $userName"
     println(welcome)
 
-    welcome = ("Добрый вечер, $userName")
+    welcome = "Добрый вечер, $userName"
     println(welcome)
-
-
-
 
 }
