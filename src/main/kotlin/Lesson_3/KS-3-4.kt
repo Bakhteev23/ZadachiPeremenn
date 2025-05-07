faab52d25b0dcdcd1e135b33d8b1795708373564
Bakhteev <Bakhteev23@inbox,ru>
2025-05-07 15:12:07 +0300

    KS-3-4

---
+++ src/main/kotlin/Lesson_3/KS-3-4.kt
@@ -0,0 +1,21 @@
+package Lesson_3
+
+fun main() {
+
+    var fromSquare = "E2"
+    var toSquare = "E4"
+    var moveNumber = 1
+
+    println("$fromSquare-$toSquare;$moveNumber")
+
+    fromSquare = "D2"
+    toSquare = "D3"
+    moveNumber = 2
+
+    println("$fromSquare-$toSquare;$moveNumber")
+
+    val nextMoveToSquare = "D3"
+    println("Следующий ход: $nextMoveToSquare")
+
+
+}
